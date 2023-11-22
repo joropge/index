5613f14aa3d8da60c2c83931969ca9272806db2f
joropge <jorgeop1997@gmail.com>
2023-11-22 08:56:10 +0100

    Se han borrado unos parentesis para que funcione la creación de bolas

--- scripts/index.js
+++ scripts/index.js
@@ -25,7 +25,7 @@ class Ball {
     draw() {
         ctx.beginPath();
         ctx.fillStyle = this.color;
-        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI());
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
         ctx.fill();
     }
 
