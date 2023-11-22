58c1d121d550e454340ffd8b7e55719a5731bbe5
joropge <jorgeop1997@gmail.com>
2023-11-22 09:21:14 +0100

    Realizada la modulación en varios js

---
+++ README.md
@@ -0,0 +1,11 @@
+**Autores: Santos, Fonseca, Laura, Caterina y Adrián**
+
+**GitHub:**
+## Estructura ##
+
+***Apartados Java Script***
+* Apartados:
+
+    1. index.js
+    2. balls-class.js
+    3. canvas-setup.js
---
+++ scripts/canvas-setup.js
@@ -0,0 +1,31 @@
+import { balls } from "./index.js";
+
+const canvas = document.querySelector('canvas');
+export const ctx = canvas.getContext('2d');
+
+export const width = canvas.width = window.innerWidth;
+export const height = canvas.height = window.innerHeight;
+
+export function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function randomRGB() {
+    return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+export function loop() {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+    ctx.fillRect(0, 0, width, height);
+  
+    for (const ball of balls) {
+      ball.draw();
+      ball.update();
+      for (const otherBall of balls) {
+        if (ball !== otherBall) {
+          ball.collisionDetect(otherBall);
+        }
+      }
+    }
+  
+    requestAnimationFrame(loop);
+  }
